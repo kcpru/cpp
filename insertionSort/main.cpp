44d97b441eb68e37622eefe32a875a8630578ab7
Kacper Ślusarczyk <kacper12345678909@gmail.com>
2019-11-13 19:57:35 +0100

    Added insertion sort

--- insertionSort/main.cpp
+++ insertionSort/main.cpp
@@ -3,31 +3,36 @@
 using namespace std;
 
 const int n = 10;
-int t[n] = {9,0,8,5,6,4,1,2,3,7};
+int t[n] = {2,8,3,9,4,0,5,6,1,7};
 
-void quicksort(int t[], int od, int dok){
-    int pp = od;
-    if(od>=dok) return;
-
-    for(int i=od; i<dok; i++){
-        if(t[i] < t[od]){
-            swap(t[i], t[pp+1]);
-            pp++;
-        }
-    }
-    swap(t[od], t[pp]);
-    quicksort(t, od, pp);
-    quicksort(t, pp+1, dok);
+void print(int t[], int n)
+{
+    for(int i=0; i<n; i++)cout<<t[i] << " ";
+    cout<<endl;
 }
 
-void print(int t[], int n){
-    for(int i=0; i<n; i++) cout << t[i] << " ";
-    cout << endl;
+void insertionSort(int t[],int n)
+{
+    int k, key, ptr;
+    for (k = 1; k < n; k++)
+    {
+        key = t[k];
+        ptr = k - 1;
+        while (ptr >= 0 && t[ptr] > key)
+        {
+            t[ptr + 1] = t[ptr];
+            ptr = ptr - 1;
+        }
+        t[ptr + 1] = key;
+    }
 }
 
 int main()
 {
-    quicksort(t, 0,n);
     print(t, n);
+
+    insertionSort(t,n);
+    print(t, n);
+
     return 0;
 }
