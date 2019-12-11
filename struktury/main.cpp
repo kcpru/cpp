c03bfd95096ca0a63ae3421f65b66134e9304846
Kacper Ślusarczyk <kacper12345678909@gmail.com>
2019-12-11 22:10:14 +0100

    Added more strings/lists/structs

--- struktury/main.cpp
+++ struktury/main.cpp
@@ -20,32 +20,31 @@ void print(osoba o)
 
 int main()
 {
-    // DEKLARACJA ZMIENNEJ TYPU OSOBA Z INIZJALIZACJ¥
+    // DEKLARACJA ZMIENNEJ TYPU OSOBA Z INIZJALIZACJI
     osoba o1 = {"Jan", "Lis", 23, 'm'};
     // NOTACJA KROPKOWA
     cout << o1.imie << " " << o1.nazwisko << endl;
     cout << o1.wiek << " " << o1.plec << endl;
 
     //---- PODSTAWIANIE STRUKTUR ----
-    // PODCZAS PODSTAWIANIA S¥ PODSTAWIANE ODPOWIADAJ¥CE SOBIE
+    // PODCZAS PODSTAWIANIA SA PODSTAWIANE ODPOWIADAJACE SOBIE
     // POLA STRUKTUR
     osoba o2;
-    o2 = {"Kacperu", "Chodok", 2, 'k'};
+    o2 = {"Imie", "Nazwisko", 27, 'k'};
     print(o2);
 
     // ---------------------------
-    // PODOBNIE JAK PROSTE ZMIENNE STRUKTRY MO¯NA TWORZYÆ
-    // DYNAMICZNIE ZA POMOC¥ NEW
+    // PODOBNIE JAK PROSTE ZMIENNE STRUKTRY MOZNA TWORZYC
+    // DYNAMICZNIE ZA POMOCA NEW
     osoba *o3;
     o3 = new osoba;
-    // DO STRUKTUR TWORZONYCH DYNAMICZNIE MO¯NA SIÊ ODNOSIÆ
-    // U¯YWAJ¥C *
+    // DO STRUKTUR TWORZONYCH DYNAMICZNIE MOZNA SIE ODNOSIC UZYWAJAC "*"
     *o3 = o1;
     cout << (*o3).imie << " " << (*o3).nazwisko << endl;
     cout << o3 -> imie << " " << o3 -> nazwisko << endl;
 
     // ODNOSZ¥C SIÊ DO PÓL STRUKTURY MO¯NA KOEZYSTAÆ
-    // BEZPOŒREDNIO ZE WSKANIKA I STRZA£KI
+    // BEZPOSREDNIO ZE WSKAZNIKA I STRZA£KI
 
     return 0;
 }
