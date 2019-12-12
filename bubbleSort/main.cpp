fd509833df5e3323460d8531f526a5cd663ec112
Kacper Ślusarczyk <kacper12345678909@gmail.com>
2019-12-13 00:11:43 +0100

    Fixes bubbleSort

--- bubbleSort/main.cpp
+++ bubbleSort/main.cpp
@@ -3,18 +3,21 @@
 using namespace std;
 
 const int n = 10;
-int t[n] = {4,3,6,8,7,9,0,1,2,5};
+int t[n] = {4, 3, 6, 8, 7, 9, 0, 1, 2, 5};
 
 void print(int t[], int n)
 {
-    for(int i=0; i<n; i++) cout << t[i] << " ";
+    for (int i = 0; i < n; i++)
+        cout << t[i] << " ";
     cout << endl;
 }
 
 void bubbleSort(int t[], int n)
 {
-    for (int i=0; i<n; i++)
-        for(int j=i; j<n; j++) if(t[j] < t[i]) swap(t[i], t[j]);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n - 1; j++)
+            if (t[j] > t[i])
+                swap(t[i], t[j]);
 }
 
 int main()
