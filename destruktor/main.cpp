3c1d13fdd6c36292b5c13924c1e6b60c7188d40c
Kacper Ślusarczyk <kacper12345678909@gmail.com>
2019-12-19 09:25:22 +0100

    Added suma, srednia

--- destruktor/main.cpp
+++ destruktor/main.cpp
@@ -8,6 +8,7 @@ class Tablica
 public:
     int rozmiar;
     int *t;
+
     Tablica(int rozmiar)
         : rozmiar(rozmiar)
     {
@@ -17,28 +18,50 @@ public:
     void losuj()
     {
         for(int i=0; i<rozmiar; i++)
-        {
             t[i] = rand() % 100;
-        }
     }
 
     int maxi()
     {
         int maxi = t[0];
-
-        for(int i=0; i<rozmiar; i++){
-            if(t[i] > maxi) maxi = t[i];
-        }
+        for(int i=0; i<rozmiar; i++)
+            if(t[i] > maxi)
+                maxi = t[i];
 
         return maxi;
+    }
+
+    int mini()
+    {
+        int mini = t[0];
+        for(int i=0; i<rozmiar; i++)
+            if(t[i] < mini)
+                mini = t[i];
+
+        return mini;
+    }
+
+    int suma()
+    {
+        int suma = 0;
+        for(int i=0; i<rozmiar; i++)
+            suma += t[i];
+
+        return suma;
+    }
+
+    int srednia()
+    {
+        int sumaElem = suma();
+        float srednia = float(sumaElem) / rozmiar;
+        return srednia;
     }
 
     void wyswietl()
     {
         for(int i=0; i<rozmiar; i++)
-        {
             cout << t[i] << " ";
-        }
+
         cout << endl;
     }
 
@@ -49,16 +72,16 @@ public:
     }
 };
 
-
-
 int main()
 {
-    cout << "Hello world!" << endl;
-    Tablica Tab1(10);
+    Tablica Tab(20);
 
-    Tab1.losuj();
-    Tab1.wyswietl();
-    cout << Tab1.maxi() << endl;
+    Tab.losuj();
+    Tab.wyswietl();
+    cout << "Max: " << Tab.maxi() << endl;
+    cout << "Min: " << Tab.mini() << endl;
+    cout << "Suma: " << Tab.suma() << endl;
+    cout << "Srednia: " << Tab.srednia() << endl;
 
     return 0;
 }
