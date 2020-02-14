73812f3cae039093ce5171c4a627095c2316747c
KacperSlusarczyk <kacper12345678909@gmail.com>
2020-02-14 08:01:46 +0100

    Update

--- zmienne_statyczne/main.cpp
+++ zmienne_statyczne/main.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+/**
+* ELEMENTY STATYCZNE ISTNIEJA OD POCZATKU WYKONYWANIA PROGRAMU
+* KONSEKWENCJA TEGO JEST ZE ISTNIEJA ZANIM ZOSTATNIE UTWORZONY OBIEKT
+* ZAWIERAJACY STATYCZNE ELEMENTY
+* MOZNA WIEC ICH UZYWAC BEZ TWORZENIA OBIEKTU
+* W PRZYPADKU ZMINNYCH STATYCZNYCH SA ONE WSPOLNE DLA WSZYSTKICH
+* OBIEKTOW TEJ KLASY
+**/
+
 class Math
 {
 public:
@@ -13,22 +22,55 @@ public:
     }
 };
 
+static float pi = 3.14;
+
+//
 class Kukulka
 {
 public:
     int kukniecTejKukulki;
 
-    // ZMIENNE STATIC ISTNIEJE ZANIM ZOSTANIE UTWORZONY JAKIKOLWIEK OBIEKT KLASY KUKULKA
+    Kukulka()
+    {
+        kukniecTejKukulki = 0;
+    }
+
+    // ZMIENNE STATIC ISTNIEJE ZANIM ZOSTANIE UTWORZONY JAKIKLWIEK OBIEKT KLASY KUKULKA
+    // CO ZATEM IDZIE NIE JEST ELEMENTEM UTWORZONYM DLA KONKTRETNEGO OBIEKTU I JEST WSPÓLNA DLA WSZYSTKICH OBIEKTÓW
     static int kukniecWszystkichKukulek;
-    void
+
+    void kuknij()
+    {
+        cout << "ku-ku" << endl;
+
+        kukniecTejKukulki++;
+        kukniecWszystkichKukulek++;
+    }
 };
 
-static float pi = 3.14;
+int Kukulka::kukniecWszystkichKukulek; // !!!!!!!!!!!!!!
 
 int main()
 {
     cout << pi << endl;
     cout << Math::square(5) << endl;
+
+    Kukulka k1, k2, k3;
+
+    k1.kuknij();
+    k1.kuknij();
+
+    k2.kuknij();
+
+    k3.kuknij();
+    k3.kuknij();
+    k3.kuknij();
+
+    cout << Kukulka::kukniecWszystkichKukulek << endl;
+
+    cout << "k1: " << k1.kukniecTejKukulki<< endl;
+    cout << "k2: " << k2.kukniecTejKukulki<< endl;
+    cout << "k3: " << k3.kukniecTejKukulki<< endl;
 
     return 0;
 }
