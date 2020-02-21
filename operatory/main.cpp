a97db9fdea551b8b271b6d02da986e70e87532b1
KacperSlusarczyk <kacper12345678909@gmail.com>
2020-02-21 08:38:27 +0100

    Update

--- operatory/main.cpp
+++ operatory/main.cpp
@@ -63,7 +63,17 @@ public:
 
         return kopia;
     }
+
+    void info()
+    {
+        cout << "(" << x << ", " << y << ", " << z << ")" << endl;
+    }
 };
+
+/**
+* Operator moze byc rowniez definiowany poza klasa a oeracja obiekt1 OP obiekt2
+* moze byc zamienona na wywolanie funkcji operatorOP(obiekt1, obiekt2)
+**/
 
 Wektor operator*(int num, Wektor w)
 {
@@ -76,22 +86,53 @@ Wektor operator*(int num, Wektor w)
     return wynik;
 }
 
-ostream& operator<<(ostream &os, Wektor w)
+//
+class Liczba
 {
-    cout << "(" << w.x << ", " << w.y << ", " << w.z << ")" << endl;
-    return os;
-}
+public:
+    int wartosc;
+
+    Liczba(int wartosc)
+    {
+        this -> wartosc = wartosc;
+    }
+
+    Liczba operator++(void)
+    {
+        wartosc += 1;
+        return wartosc;
+    }
+
+    void info()
+    {
+        cout << wartosc << endl;;
+    }
+};
 
 int main()
 {
     Wektor w1(1,2,3), w2(4,5,6), w3(0,0,0), w4(0,0,1);
 
-    cout << w1 << w2 << w3;
-    w1++; w2++; w3++; w4++;
+    w3 = w1 + w2;
+    w3.info();
 
+    w3 = w3 * 2;
+    w3.info();
 
-    cout << endl;
-    cout << w1 << w2 << w3 << w4;
+    w3 = 2 * w3;
+    w3.info();
+
+    ++w4;
+    w4.info();
+
+    w4++;
+    w4.info();
+
+    Liczba l1(1), l2(0);
+    ++l1;
+    l2 = ++l1;
+    l1.info();
+    l2.info();
 
     return 0;
 }
