26aafff2b7a1d312be36ff6b8758f332df5829d0
Kacper Slusarczyk <kacper12345678909@gmail.com>
2020-04-24 09:31:00 +0200

    Update

--- polimorfizm/main.cpp
+++ polimorfizm/main.cpp
@@ -9,7 +9,7 @@ int suma(int a, int b)
 
 int iloczyn(int a, int b)
 {
-    return a + b;
+    return a * b;
 }
 
 int suma(int a, int b, int c)
@@ -22,18 +22,28 @@ string suma (string s1, string s2)
     return s1 + s2;
 }
 
+void call(int (*f)(int, int))
+{
+    cout << f(2,3) << endl;
+}
+
 int main()
 {
     cout << suma(1,2) << endl;
     cout << suma(3,4,5) << endl;
     cout << suma("hop","siup") << endl;
 
-    int (*f)(int, int );
+    //
+    int (*f)(int, int);
 
-    f=suma;
-
+    f = suma;
     cout << f(2,3) << endl;
 
+    f = iloczyn;
+    cout << f(2,3) << endl;
+
+    call(suma);
+    call(iloczyn);
 
     return 0;
 }
