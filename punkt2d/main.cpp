341ecc5c6da81e52da4f1c7911822d7d677e769d
Kacper Slusarczyk <kacper12345678909@gmail.com>
2020-04-24 08:59:00 +0200

    Added polimorfizm

--- punkt2d/main.cpp
+++ punkt2d/main.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-
 class Punkt2d
 {
 public:
@@ -19,7 +18,7 @@ public:
     }
 };
 
-class Punkt3d : Punkt2d
+class Punkt3d : public Punkt2d
 {
 public:
     int z;
@@ -48,10 +47,28 @@ int main()
     p2d2.info();
 
 
-    Punkt3d p3d1(1,2,3), p3d2(3,4,5), p3d3(0,0,0);
+    Punkt3d p3d1(3,4,5);
     p3d1.info();
-    p3d2.info();
-    p3d3.info();
+
+    p2d1 = p3d1; // Rzutowanie w gore jest DOZWOLONE
+    p2d1.info();
+
+    // p3d1 = p2d1; // Rzutowanie w dol na klase pochodna jest ZABRONIONE
+
+    Punkt2d *wp2d = new Punkt2d(1,2);
+    Punkt3d *wp3d = new Punkt3d(7,8,9);
+
+    wp2d = wp3d;
+    wp2d -> info();
+
+
+    Punkt2d *wp2d2 = new Punkt2d(1,2);
+    Punkt3d *wp3d2 = new Punkt3d(7,8,9);
+
+    // wp3d2=wp2d2; // Standardowo nie DZIA£A
+    wp3d2 = (Punkt3d*) wp2d2; // Mozna wymusic jawne rzutowanie...
+    wp3d2 -> info(); // ...ale moze doprowadzic to nieprzewidywalnych efektow: Drukuje z ktorego nie ma
+
 
     return 0;
 }
