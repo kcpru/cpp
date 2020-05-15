6732844d3df0a68dd6457b59cce67d608e696317
Kacper Slusarczyk <kacper12345678909@gmail.com>
2020-05-15 09:02:33 +0200

    Update

--- pliki/main.cpp
+++ pliki/main.cpp
@@ -9,11 +9,32 @@ using namespace std;
 int main()
 {
     fstream f;
+
+    // Wpisywanie do pliku
     f.open("./plik.txt", ios_base::out);
 
     f << "Jan lis limanowa" << endl;
-    f << "Janina lisek laskowa" << endl;
-    f << "Janusz lisowski lososina" << endl;
+    f << "Janina lisek laskowa gorna" << endl;
+    f << "Janusz lisowski lososina"; // Za ostatnim elementem nie ma prawa byc niczego
+
+    f.close();
+
+    // Czytanie z pliku
+    f.open("./plik.txt", ios_base::in);
+
+    char tekst[25];
+
+    while(!f.eof()) // Dopoki nie ma konca pliku
+    {
+
+        f >> tekst;
+        cout << "Imie: " << tekst << endl;
+        f >> tekst;
+        cout << "Nazwisko: " << tekst << endl;
+        f.getline(tekst, 25);
+        cout << "Miejscowosc: " << tekst << endl;
+        cout << endl;
+    }
 
     f.close();
 
